6a8efacc9305392f247546f2d345638f9fc7583c
guyue <myoldmoons@163.com>
2020-06-15 18:02:47 +0800

    add loading

--- src/component/list.svelte
+++ src/component/list.svelte
@@ -2,12 +2,18 @@
   import ajax from "../utils/http.js";
   import Header from "./common/header.svelte";
   import { onMount } from "svelte";
+  import Loading from "./common/loading.svelte";
   let list = [];
+  let getTitle = null;
+  let total=[1,2];
   onMount(() => {
     ajax.get("/photos?_limit=10").then(res => {
       console.log(res);
       list = res;
     });
+    setTimeout(() => {
+      getTitle = "局部loading。。。";
+    }, 1500);
   });
 </script>
 
@@ -15,17 +21,23 @@
   section {
     padding: 0 0.5rem;
   }
-  ul{
-      display: flex;
-      flex-wrap: wrap;
-      justify-content: space-between;
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
   }
-  ul li{
-      width:32%;
-      margin-bottom: 6px;
+  ul li {
+    width: 32%;
+    margin-bottom: 6px;
   }
-  ul li img{
-      width: 100%;
+  ul li img {
+    width: 100%;
+  }
+  .title {
+    width: 100%;
+    height: 50px;
+    text-align: center;
+    line-height: 50px;
   }
 </style>
 
@@ -35,6 +47,15 @@
   </header>
 
   <section>
+    {#each total as i}
+      <div class="title">
+        {#if getTitle}
+          {getTitle}
+        {:else}
+          <Loading />
+        {/if}
+      </div>
+    {/each}
     <ul>
       {#each list as item}
         <li>
